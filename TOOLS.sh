dc6f9bc6dfecc13432d61d33442099a220438f80
F002-F002 <66986030+F002-F002@users.noreply.github.com>
2020-06-17 14:52:06 +0700

    Add files via upload

---
+++ TOOLS.sh
@@ -0,0 +1,257 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+figlet   Tools F002 | lolcat
+echo
+echo  " ____________________-=[Private Tools]=-____________________" | lolcat
+echo  "|                                                           |" | lolcat
+echo  "|              AUTHOR          : F002                       |" | lolcat
+echo  "|              FACEBOOK        : Onii chan                  |" | lolcat
+echo  "|              NO WA           : 0895422865135              |" | lolcat
+echo  "|___________________________________________________________|" | lolcat
+date | lolcat
+echo
+echo $i"========================================"
+echo $i"|"$me" 0"$i" |"$me" INSTALL BAHAN"
+echo $i"========================================"
+echo $i"|"$me" 1"$i" |"$ku" SPAM SMS INDIHOME"
+echo $i"|"$me" 2"$i" |"$cy" SPAM WA TOKOPEDIA"
+echo $i"|"$me" 3"$i" |"$pu" TOOLS KING HACKING"
+echo $i"|"$me" 4"$i" |"$bi" HACK WIFI"
+echo $i"|"$me" 5"$i" |"$i" TERMUX THEME"
+echo $i"|"$me" 6"$i" |"$pu" HACK FB NO CP"
+echo $i"────────────────────────────────"
+echo $i"|"$me" 7"$i" |"$cy" YAHOO CLONING FACEBOOK"
+echo $i"|"$me" 8"$i" |"$ku" PROFILE GUARD FACEBOOK"
+echo $i"|"$me" 9"$i" |"$bi" AUTO REPORT FACBOOK"
+echo $i"|"$me"10"$i" |"$i" AUTO REACTION FACEBOOK"
+echo $i"|"$me"11"$i" |"$pu" AUTO LIKE FACEBOOK"
+echo $i"|"$me"12"$i" |"$me" AUTO KOMEN FACEBOOK"
+echo $i"|"$me"13"$i" |"$ku" INFORMATION FACEBOOK"
+echo $i"────────────────────────────────"
+echo $i"|"$me"14"$i" |"$i" PHISING FACEBOOK 1"
+echo $i"|"$me"15"$i" |"$bi" PHISING FACEBOOK 2"
+echo $i"|"$me"16"$i" |"$bu" PHISING FACEBOOK 3"
+echo $i"|"$me"17"$i" |"$pu" PHISING FACEBOOK 4"
+echo $i"|"$me"18"$i" |"$
+echo $i"========================================"
+echo $i"|"$me"18"$i" |"$pu" EXIT"
+echo $i"========================================"
+echo
+echo $me"┌==="$bi"["$i"002"$bi"]"$me"======"$bi"["$i""enter number""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 0 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " INSTALLATION SUCCES "|lolcat
+fi
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/blackcodercrush/spam-indihome
+cd spam-indihome
+bash spam.sh
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/blackcodercrush/spam-whatsapp-tokopedia
+cd spam-whatsapp-tokopedia
+bash wa.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/pashayogi/Trojan
+cd Trojan
+
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+chmod +x wifi-hacker.sh
+./wifi-hacker.sh
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone http://github.com/F002-F002/termux-theme
+cd FaceBrute
+python fb.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/BangDanz/DarkMaster
+cd DarkMaster
+python2 darkmaster.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/AMVengeance/FB-React
+cd FB-React
+./start
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/InfoFB
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/blackeye.git
+cd blackeye
+bash blackeye.sh
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "WAIT"|lolcat
+sleep 1
+https://github.com/evait-security/weeman.git
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $pil = 18 ]
+then
+clear
+figlet -f slant "SHUTDOWN"|lolcat
+sleep 2
+echo $cy"Thanks For Using This Tools"
+sleep 2
+echo $pur"DON'T SHARE MY TOOLS"
+exit
+fi
+
